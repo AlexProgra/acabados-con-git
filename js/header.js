83f071113bba21b4a51ff787109431558436ae01
alex26progra <alexprograma26@gmail.com>
2022-02-19 21:12:06 -0500

    guardando mini cambios

--- js/header.js
+++ js/header.js
@@ -1,4 +1,4 @@
 window.addEventListener('scroll', function() {
     var header = document.querySelector("header");
-    header.classList.toggle("fijar-header",window.scrollY > 150)
+    header.classList.toggle("fijar-header",window.scrollY > 10)
 });
